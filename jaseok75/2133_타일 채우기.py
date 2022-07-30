4653bb6fabedb9b02c1487c8b23eb62199c61658
Harim Kim <jaseok75@gmail.com>
2022-07-31 02:03:38 +0900

    타일 채우기_골드4_7시간
    https://www.acmicpc.net/problem/2133

---
+++ jaseok75/2133_타일 채우기.py
@@ -0,0 +1,60 @@
+'''
+Project: 백준_2133
+Date: 2022.07.31.
+Content: DP
+- 짝수만 가능 (홀수일 때는 0으로 반환)
+- 현재값 = 이전값 * 3 + (이전값 - 이전이전값)
+'''
+
+
+def solution():
+    if n % 2 != 0:
+        print("0")
+        return
+    result_1, result_2 = 1, 0 # 최초의 값은 1로 설정
+    diff = 0    # 이전값 - 이전이전값
+    for i in range(n // 2):
+        result_2 = result_1 * 3 # 현재값 = 이전값 * 3
+        result_2 += diff        # 현재값 += (이전값 - 이전이전값)
+        diff = result_2 - result_1  # 차이값 바꿔주기
+        result_1 = result_2 # 이전값 바꿔주기
+    print(result_2)
+
+
+if __name__ == "__main__":
+    n = int(input())
+    solution()
+
+
+'''
+Project: 백준_17298
+Date: 2022.07.31.
+Content: DP
+- A 수열의 수를 하나씩 q에 넣는다.
+- 넣는 수보다 오른쪽의 수가 더 크면 그건 오큰수이므로 그걸 오큰수로 지정한다.
+- q에 넣어진 수를 순서대로 확인하며 찾은 오큰수로 지정할 수 있는 것들을 결정한다.
+'''
+
+
+from collections import deque
+
+
+def solution():
+    NGE = [-1 for _ in range(n)]    # 일단 모든 값은 -1로 초기화
+    q = deque()
+    for i in range(1, n):
+        q.append((map_A[i-1], i-1)) # 이전 값이랑 이전 위치 저장 (오른쪽 값이 더 클 때까지)
+        if map_A[i - 1] < map_A[i]: # 오른쪽 값이 더 크면
+            num = map_A[i]  # 그게 오큰수
+            while q:
+                if q[-1][0] < num:  # 찾은 오큰수로 q에서 뺄 수 있는거 다 빼서
+                    NGE[q.pop()[1]] = num   # 그걸 오큰수로 채우기
+                else:   # 근데 찾은 오큰수보다 q에 들어있는 게 더 크면 그건 오큰수 해당 안되니까
+                    break   # 그만하고 나가기
+    print(*NGE)
+
+
+if __name__ == "__main__":
+    n = int(input())
+    map_A = list(map(int, input().split()))
+    solution()
